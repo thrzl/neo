3a4c27794ea5b517ec2e69bb7a8c6658c9e04b9b
te <thrzl@icloud.com>
2025-04-03 08:51:08 -0400

    fix link and change placeholder to real image

--- src/components/MusicStats.svelte
+++ src/components/MusicStats.svelte
@@ -47,12 +47,12 @@
         <p>Total Releases: {stats.total_release_count}</p>
         <ul class="grid auto-rows-max lg:grid-cols-3 gap-0">
             {#each stats.releases as release}
-                <a href="https://wsrv.nl/?url=listenbrainz.org/release/{release.release_mbid}" class="relative overflow-hidden inline-block md:min-w-48 group aspect-ratio-square b-2 b-black p-2 hover:p-0 duration-300 cursor-help">
+                <a href="https://listenbrainz.org/release/{release.release_mbid}" class="relative overflow-hidden inline-block md:min-w-48 group aspect-ratio-square b-2 b-black p-2 hover:p-0 duration-300 cursor-help">
                     <!-- <strong>{release.release_name}</strong> by {release.artist_name} 
                     (Listens: {release.listen_count}) -->
                     <img src="//coverartarchive.org/release/{release.release_mbid}/front-500" 
                          alt="{release.release_name} cover art" 
-                         on:error="{(e) => e.target.src = 'https://via.placeholder.com/250?text=No+Cover+Art'}"
+                         on:error="{(e) => e.target.src = '/music.avif'}"
                          class="w-full h-auto mb-2 group-hover:scale-100 group-hover:brightness-30 group-hover:blur-none transition duration-300" />
                     <!-- show title and artist in center on hover, and darken background -->
                     <div class="absolute bottom-0 op-0 group-hover:op-100 transition-delay-150 transition-200 w-full h-full z-1 flex justify-center items-center flex-col lowercase">
