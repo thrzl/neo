da10b8addacd38204c271b4e245b4c27bb79398f
te <thrzl@icloud.com>
2025-04-04 17:48:35 -0400

    improve consistency and small screen size display

--- src/components/MusicStats.svelte
+++ src/components/MusicStats.svelte
@@ -45,19 +45,21 @@
     <div class="music-stats">
         <h2>Music Stats</h2>
         <p>Total Releases: {stats.total_release_count}</p>
-        <ul class="grid auto-rows-max lg:grid-cols-3 gap-0">
-            {#each stats.releases as release}
-                <a href="https://listenbrainz.org/release/{release.release_mbid}" class="relative overflow-hidden inline-block md:min-w-48 group aspect-ratio-square b-2 b-black p-2 hover:p-0 duration-300 cursor-help">
+        <!-- if there has to be a large item; it'll now appear at the top. wrap-reverse means it flows backwards,
+         and reversing the list essentially undoes it -->
+        <ul class="flex flex-wrap-reverse flex-row gap-0 w-full justify-center">
+            {#each [...stats.releases].reverse() as release, i}
+                <a href="https://listenbrainz.org/release/{release.release_mbid}" class="order-{i} relative overflow-hidden inline-block min-w-48 grow basis-48 group aspect-ratio-square b-2 b-black p-2 hover:p-0 duration-300 cursor-help">
                     <!-- <strong>{release.release_name}</strong> by {release.artist_name} 
                     (Listens: {release.listen_count}) -->
-                    <img src="//coverartarchive.org/release/{release.release_mbid}/front-500" 
+                    <img src="https://wsrv.nl/?url=coverartarchive.org/release/{release.release_mbid}/front-500" 
                          alt="{release.release_name} cover art" 
                          on:error="{(e) => e.target.src = '/music.avif'}"
                          class="w-full h-auto mb-2 group-hover:scale-100 group-hover:brightness-30 group-hover:blur-none transition duration-300" />
                     <!-- show title and artist in center on hover, and darken background -->
                     <div class="absolute bottom-0 op-0 group-hover:op-100 transition-delay-150 transition-200 w-full h-full z-1 flex justify-center items-center flex-col lowercase">
                         <p class="text-sm text-neutral-300 text-center w-full">{release.artist_name}</p>
-                            <p class="text-white text-lg lg:text-2xl !line-height-none font-800 text-center w-4/5">{release.release_name}</p>
+                            <p class="text-white text-lg lg:text-4xl !line-height-none font-800 text-center w-4/5">{release.release_name}</p>
                     </div>
                     <!-- <div class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 text-sm">
                         <p>{release.release_name}</p>
