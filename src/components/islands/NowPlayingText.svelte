d8b157c444db90b66d5dcbb3c3b4438981fcdb87
te <thrzl@icloud.com>
2025-04-20 00:17:56 -0400

    fix trailing double period with some artist names

--- src/components/islands/NowPlayingText.svelte
+++ src/components/islands/NowPlayingText.svelte
@@ -14,5 +14,5 @@
 {#await recentTrack}
     <p>loading music data...</p>
 {:then { track, now_playing }}
-	<p class="text-lg">{now_playing ? "now playing: ": "recently played"} {cleanTrackString(track.track_name.toRespectfulLowerCase())} — {track.artist_name.toRespectfulLowerCase()}.</p>
+	<p class="text-lg">{now_playing ? "now playing: ": "recently played"} {cleanTrackString(track.track_name.toRespectfulLowerCase())} — {track.artist_name.toRespectfulLowerCase()}{track.artist_name.slice(-1) !== "." ? "." : ""}</p>
 {/await}
