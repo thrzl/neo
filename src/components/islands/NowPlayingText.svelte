490f37d2ad29b45a1371b8470039d62e73542827
te <thrzl@icloud.com>
2025-04-20 00:08:55 -0400

    add more traditional index page

---
+++ src/components/islands/NowPlayingText.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import getRecentTrack from "../../lib/listenbrainz";
+	import "../../lib/helpers"
+
+	const recentTrack = getRecentTrack();
+
+    function cleanTrackString(text: string) {
+        return text
+            .replace(/\s*-\s*[^-]+$/, "") // remove '- EP', '- Single', etc.
+            .replace(/\s*\(feat\. [^)]+\)/i, ""); // remove '(feat. ...)'
+    }
+</script>
+
+{#await recentTrack}
+    <p>loading music data...</p>
+{:then { track, now_playing }}
+	<p class="text-lg">{now_playing ? "now playing: ": "recently played"} {cleanTrackString(track.track_name.toRespectfulLowerCase())} — {track.artist_name.toRespectfulLowerCase()}.</p>
+{/await}
