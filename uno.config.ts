ef63e3a69693aaf0a138cb819c757762ea749cfa
te <thrzl@icloud.com>
2025-04-22 22:37:18 -0400

    add divider unocss class

--- uno.config.ts
+++ uno.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'unocss'
 
 export default defineConfig({
   shortcuts: {
-    "link": "duration-100 hover:b-b-1 b-b-[--accent-bg-light] b-b-dotted cursor-help text-[--accent-muted-light]"
+    "link": "duration-100 hover:b-b-1 b-b-[--accent-bg-light] b-b-dotted cursor-help text-[--accent-muted-light]",
+    "divider": "b-green-8 b-dotted b-8 b-b-0 h-0 p-0 my-8 w-1/4 mx-auto",
   },
 })
