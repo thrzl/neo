de25b8e6d5dd511e2af8cdb45835248366d48b35
te <thrzl@icloud.com>
2025-04-22 22:37:27 -0400

    add transformers to unocss config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro'
+import { transformerDirectives } from 'unocss'
 
 import svelte from '@astrojs/svelte';
 
@@ -8,6 +9,7 @@ import svelte from '@astrojs/svelte';
 export default defineConfig({
   integrations: [svelte(), UnoCSS({
     injectReset: true,
+    transformers: [transformerDirectives()]
   })],
   site: "https://thrizzle.nekoweb.org",
 });
