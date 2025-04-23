c9e66bc0163854a4442da401262c2e0f08b41b2a
te <thrzl@icloud.com>
2025-04-23 21:25:30 +0000

    update navbar + wip mastodon status

---
+++ src/components/islands/MastodonRecent.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    type MastodonPost = {
+        createdAt: string;
+        content: string;
+        url: string;
+    };
+
+    function htmlDecode(input: string) {
+        const doc = new DOMParser().parseFromString(input, "text/html");
+        return doc.documentElement.textContent;
+    }
+
+    async function getStatus() {
+        const res = await fetch(
+            "https://wetdry.world/api/v1/accounts/114383870712581026/statuses?exclude_replies=true&exclude_reblogs=true",
+        );
+        if (!res.ok) {
+            throw new Error("Network response was not ok");
+        }
+        // console.log(await res.json())
+        const raw_data: MastodonPost[] = await res.json();
+        return raw_data[0];
+    }
+    const status = getStatus();
+
+    // map emoji to public/skype images
+    const emoji_map = {
+        "🙂": "slightly_smiling_face",
+        "😐": "neutral_face",
+        "😎": "smiling_face_with_sunglasses",
+        "🥰": "smiling_face_with_hearts",
+        "😛": "face_savoring_food",
+        "😂": "rolling_on_the_floor_laughing",
+        "❤️": "red_heart",
+        "💔": "broken_heart",
+        "😇": "smiling_face_with_halo",
+        "☀️": "star",
+        "🥱": "yawning_face",
+    };
+</script>
+
+{#await status}
+<span class="font-800 pr-1 mr-1 b-r-1 b-r-solid">loading &nbsp;</span>
+<p class="inline">
+    please wait...
+    <img
+            class="h-4 inline align-middle"
+            height="16px"
+            src="/skype/mantelpiece_clock.png"
+            alt="clock emoji"
+        />
+</p>
+{:then status}
+    <span class="font-800 pr-1 mr-1 b-r-1 b-r-solid text-2xl lg:text-lg">{status.createdAt}&nbsp;</span>
+    <p class="inline text-2xl lg:text-lg">
+        {htmlDecode(status.content)}
+        <!-- {#if emoji_map.hasOwnProperty(status.face)}<img
+                class="h-4 inline align-middle"
+                height="16px"
+                src={`/skype/${emoji_map[status.face]}.png`}
+                alt={status.face.replace("_", " ")}
+            />{/if} -->
+    </p>
+{:catch error}
+    <div class="text-red-500">Error: {error.message}</div>
+{/await}
