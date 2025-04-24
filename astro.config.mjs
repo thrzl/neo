5e9e589d39dc086852f0a43fed9e06434e856956
te <thrzl@icloud.com>
2025-04-23 20:58:05 -0400

    build sitemaps now

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import { transformerDirectives } from 'unocss'
 
 import svelte from '@astrojs/svelte';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), UnoCSS({
     injectReset: true,
     transformers: [transformerDirectives()]
-  })],
+  }), sitemap()],
   site: "https://thrizzle.nekoweb.org",
 });
