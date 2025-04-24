18ae5ae7eaedab409cecbf010a5dd4b1001573a2
te <thrzl@icloud.com>
2025-04-23 22:42:15 -0400

    make links page pull from data file

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -11,3 +11,27 @@ export const personalLinks = [
     { title: "website source", url: "https://github.com/thrzl/neo"},
     {title: "listenbrainz", url: "https://listenbrainz.org/user/thrizzle/"}
 ]
+
+export const linksPage = {
+    design_stuff: [
+        {title: "cari institute", description: "the consumer aesthetics research institute website, a catalog of various design eras", url: "https://cari.institute/"},
+        {title: "dafont", description: "massive free font catalog", url: "https://www.dafont.com/"},
+        {title: "fontshare", description: "another (smaller) free font catalog; tends to focus on more modern fonts", url: "https://fontshare.com/"},
+        {title: "typerip", description: "adobe fonts ripper. for testing purposes. obviously", url: "https://badnoise.net/TypeRip/"},
+        {title: "macosicons", description: "exactly what it sounds like; over 26k macOS app icons (disclaimer: some are unofficial)"},
+        {title: "what the font", description: "font detector. im not gon hold u the effectiveness depends on the day", url: "https://www.myfonts.com/pages/whatthefont"},
+    ],
+    apps_services: [
+        {title: "miruro", description: "pretty solid anime site", url: "https://www.miruro.to/"},
+        {title: "anki", description: "the absolute BEST srs (spaced repitition system) ever. great form of digital flash cards", url: "https://apps.ankiweb.net/"},
+        {title: "tinywow", description: "'free pdf editor online', 'free png to jpg converter online', etc, all in one place, with no ads or anything. 10/10", url: "https://tinywow.com/"},
+        {title: "shrib", description: "online notepad that syncs based on ur slug/id. pretty nice", url: "https://shrib.com/"},
+        {title: "cobalt", description: "high quality video/audio downloader from a variety of social media sites", url: "https://cobalt.tools/"},
+        {title: "ytdlp-online", description: "for when cobalt doesn't cut it. supports basically every website ever", url: "https://ytdlp.online/"},
+        
+    ],
+    web_dev: [
+        {title: "squoosh", description: "compresses images super well to a ton of different formats", url: "https://squoosh.app/"},
+        {title: "fontie", description: "super cool webfont generator that i discovered while working on this site. has tons of conversion options", url: "https://fontie.pixelsvsbytes.com/webfont-generator"}
+    ]
+}
