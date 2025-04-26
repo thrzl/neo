ca4c98b50ebdd0d69b1731ee5df2c4770e969d7a
te <thrzl@icloud.com>
2025-04-26 13:31:36 -0400

    make rss feed image urls absolute

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,19 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content';
+import {JSDOM} from "jsdom";
+
+function makeAbsoluteUrls(html: string, baseUrl: URL) {
+    const {document: doc}: {document: Document} = (new JSDOM(html)).window
+
+    const imgsRaw: HTMLCollection = doc.getElementsByTagName("img")
+    for (const img of imgsRaw) {
+      if (img.src && !img.src.startsWith(baseUrl)) {
+        img.src = new URL(img.src, baseUrl.href).href
+      }
+    }
+    return doc.documentElement.outerHTML
+  }
+  
 
 export async function GET({ site }: { site: string }) {
     const blogPosts = (await getCollection('blog')).filter(post => !post.data.draft)
@@ -7,6 +21,7 @@ export async function GET({ site }: { site: string }) {
         title: "thrizzle's garden",
         description: "my hopes, dreams, thoughts and my schemes",
         site: site,
-        items: blogPosts.map((post) => ({ title: post.data.title, description: post.data.description, pubDate: post.data.date, link: `garden/${post.id}`, content: post.rendered?.html })),
+        trailingSlash: false,
+        items: blogPosts.map((post) => ({ title: post.data.title, description: post.data.description, pubDate: post.data.date, link: `garden/${post.id}`, content: makeAbsoluteUrls(post.rendered.html, site) })),
     })
 }
