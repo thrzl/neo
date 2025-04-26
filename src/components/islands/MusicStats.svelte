320e7961d5acfc6fe9ef9af44c6330f769436ffe
te <thrzl@icloud.com>
2025-04-26 14:14:36 -0400

    higher resolution image when it's going to be large

--- src/components/islands/MusicStats.svelte
+++ src/components/islands/MusicStats.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     export let range: string;
-    import "../../lib/helpers"
+    import "../../lib/helpers";
     type Stats = {
         count: number;
         from_ts: number;
@@ -45,29 +45,51 @@
     <div class="music-stats">
         <ul class="flex flex-wrap-reverse flex-row gap-0 w-full justify-center">
             {#each [...stats.releases].reverse() as release, i}
-                <a href="https://listenbrainz.org/release/{release.release_mbid}" class="order-{i} relative overflow-hidden inline-block min-w-64 grow basis-64 group aspect-ratio-square  duration-300 cursor-help">
-                    <!-- <strong>{release.release_name}</strong> by {release.artist_name} 
-                    (Listens: {release.listen_count}) -->
-                    <img src="https://wsrv.nl/?url=coverartarchive.org/release/{release.release_mbid}/front-500" 
-                         alt="{release.release_name} cover art"
-                         on:error="{(e) => e.target.src = '/music.avif'}"
-                         class="w-full h-auto group-hover:m-0 scale-95 group-hover:scale-100 group-hover:blur-none transition duration-300"
-                         loading="lazy" />
+                <a
+                    href="https://listenbrainz.org/release/{release.release_mbid}"
+                    class="order-{i} relative overflow-hidden inline-block min-w-64 grow basis-64 group aspect-ratio-square duration-300 cursor-help"
+                >
+                    <img
+                        src="https://wsrv.nl/?url=coverartarchive.org/release/{release.release_mbid}/front-{i ===
+                            stats.releases.length - 1 &&
+                        stats.releases.length % 2 !== 0
+                            ? 1200
+                            : 500}"
+                        alt="{release.release_name} cover art"
+                        on:error={(e) => (e.target.src = "/music.avif")}
+                        class="w-full h-auto group-hover:m-0 scale-95 group-hover:scale-100 group-hover:blur-none transition duration-300"
+                        loading={i === stats.releases.length - 1 &&
+                        stats.releases.length % 2 !== 0
+                            ? "eager"
+                            : "lazy"}
+                    />
+
                     <!-- show title and artist in center on hover, and darken background -->
-                    <div class="absolute bg-[--accent-muted-dark] top-0 op-0 group-hover:op-80 scale-95 group-hover:scale-100 duration-200 w-full h-full z-1"></div>
-                    <div class="absolute bottom-0 op-0 group-hover:op-100 transition-delay-150 transition-200 w-full h-full z-2 flex justify-center items-center flex-col">
-                        <p class="text-sm text-neutral-300 text-center w-full italic">{release.artist_name.toRespectfulLowerCase().replaceAll("’", "'")}</p>
-                            <p class="text-white text-lg lg:text-4xl !line-height-none font-800 text-center w-4/5">{release.release_name.toRespectfulLowerCase().replaceAll("’", "'")}</p>
+                    <div
+                        class="absolute bg-[--accent-muted-dark] top-0 op-0 group-hover:op-80 scale-95 group-hover:scale-100 duration-200 w-full h-full z-1"
+                    ></div>
+                    <div
+                        class="absolute bottom-0 op-0 group-hover:op-100 transition-delay-150 transition-200 w-full h-full z-2 flex justify-center items-center flex-col"
+                    >
+                        <p
+                            class="text-sm text-neutral-300 text-center w-full italic"
+                        >
+                            {release.artist_name
+                                .toRespectfulLowerCase()
+                                .replaceAll("’", "'")}
+                        </p>
+                        <p
+                            class="text-white text-lg lg:text-4xl !line-height-none font-800 text-center w-4/5"
+                        >
+                            {release.release_name
+                                .toRespectfulLowerCase()
+                                .replaceAll("’", "'")}
+                        </p>
                     </div>
-                    <!-- <div class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 text-sm">
-                        <p>{release.release_name}</p>
-                        <p>{release.artist_name}</p>
-                    </div> -->
                 </a>
             {/each}
         </ul>
     </div>
 {:catch error}
     <p>Error loading music stats: {error.message}</p>
-    
 {/await}
