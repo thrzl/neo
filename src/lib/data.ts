b9c45f67d4c699ef7726d5bcfadc6e8a21cbd01b
te <thrzl@icloud.com>
2025-06-16 21:02:36 -0400

    update links logic and add a few extras

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,105 +1,247 @@
 export const navBarLinks = [
-	{ name: "home", slug: "/" },
-	{ name: "garden", slug: "/garden" },
-	{ name: "tunes (img heavy!)", slug: "/tunes" },
-	{ name: "cool places", slug: "/links" },
+  { name: "home", slug: "/" },
+  { name: "garden", slug: "/garden" },
+  { name: "tunes (img heavy!)", slug: "/tunes" },
+  { name: "cool places", slug: "/links" },
 ];
 
 export const personalLinks = [
-	{ title: "mastodon", url: "https://wetdry.world/@thrizzle" },
-	{ title: "github", url: "https://github.com/thrzl" },
-	{
-		title: "website source",
-		url: "https://github.com/thrzl/neo",
-		rel: "external",
-	},
-	{ title: "listenbrainz", url: "https://listenbrainz.org/user/thrizzle/" },
-	{ title: "spacehey", url: "https://spacehey.com/thrizzle" },
+  { title: "mastodon", url: "https://wetdry.world/@thrizzle" },
+  { title: "github", url: "https://github.com/thrzl" },
+  {
+    title: "website source",
+    url: "https://github.com/thrzl/neo",
+    rel: "external",
+  },
+  { title: "listenbrainz", url: "https://listenbrainz.org/user/thrizzle/" },
+  { title: "spacehey", url: "https://spacehey.com/thrizzle" },
 ];
 
 export const linksPage = {
-	design_stuff: [
-		{
-			title: "cari institute",
-			description:
-				"the consumer aesthetics research institute website, a catalog of various design eras",
-			url: "https://cari.institute/",
-		},
-		{
-			title: "dafont",
-			description: "massive free font catalog",
-			url: "https://www.dafont.com/",
-		},
-		{
-			title: "fontshare",
-			description:
-				"another (smaller) free font catalog; tends to focus on more modern fonts",
-			url: "https://fontshare.com/",
-		},
-		{
-			title: "typerip",
-			description: "adobe fonts ripper. for testing purposes. obviously",
-			url: "https://badnoise.net/TypeRip/",
-		},
-		{
-			title: "macosicons",
-			description:
-				"exactly what it sounds like; over 26k macOS app icons (disclaimer: some are unofficial)",
-		},
-		{
-			title: "what the font",
-			description:
-				"font detector. im not gon hold u the effectiveness depends on the day",
-			url: "https://www.myfonts.com/pages/whatthefont",
-		},
-	],
-	apps_services: [
-		{
-			title: "miruro",
-			description: "pretty solid anime site",
-			url: "https://www.miruro.to/",
-		},
-		{
-			title: "anki",
-			description:
-				"the absolute BEST srs (spaced repitition system) ever. great form of digital flash cards",
-			url: "https://apps.ankiweb.net/",
-		},
-		{
-			title: "tinywow",
-			description:
-				"'free pdf editor online', 'free png to jpg converter online', etc, all in one place, with no ads or anything. 10/10",
-			url: "https://tinywow.com/",
-		},
-		{
-			title: "shrib",
-			description: "online notepad that syncs based on ur slug/id. pretty nice",
-			url: "https://shrib.com/",
-		},
-		{
-			title: "cobalt",
-			description:
-				"high quality video/audio downloader from a variety of social media sites",
-			url: "https://cobalt.tools/",
-		},
-		{
-			title: "ytdlp-online",
-			description:
-				"for when cobalt doesn't cut it. supports basically every website ever",
-			url: "https://ytdlp.online/",
-		},
-	],
-	web_dev: [
-		{
-			title: "squoosh",
-			description: "compresses images super well to a ton of different formats",
-			url: "https://squoosh.app/",
-		},
-		{
-			title: "fontie",
-			description:
-				"super cool webfont generator that i discovered while working on this site. has tons of conversion options",
-			url: "https://fontie.pixelsvsbytes.com/webfont-generator",
-		},
-	],
+  design_stuff: [
+    {
+      title: "cari institute",
+      description:
+        "the consumer aesthetics research institute website, a catalog of various design eras",
+      url: "https://cari.institute/",
+    },
+    {
+      title: "dafont",
+      description: "massive free font catalog",
+      url: "https://www.dafont.com/",
+    },
+    {
+      title: "fontshare",
+      description:
+        "another (smaller) free font catalog; tends to focus on more modern fonts",
+      url: "https://fontshare.com/",
+    },
+    {
+      title: "typerip",
+      description: "adobe fonts ripper. for testing purposes. obviously",
+      url: "https://badnoise.net/TypeRip/",
+    },
+    {
+      title: "macosicons",
+      description:
+        "exactly what it sounds like; over 26k macOS app icons (disclaimer: some are unofficial)",
+    },
+    {
+      title: "what the font",
+      description:
+        "font detector. im not gon hold u the effectiveness depends on the day",
+      url: "https://www.myfonts.com/pages/whatthefont",
+    },
+  ],
+  apps_services: [
+    {
+      title: "miruro",
+      description: "pretty solid anime site",
+      url: "https://www.miruro.to/",
+    },
+    {
+      title: "anki",
+      description:
+        "the absolute BEST srs (spaced repitition system) ever. great form of digital flash cards",
+      url: "https://apps.ankiweb.net/",
+    },
+    {
+      title: "tinywow",
+      description:
+        "'free pdf editor online', 'free png to jpg converter online', etc, all in one place, with no ads or anything. 10/10",
+      url: "https://tinywow.com/",
+    },
+    {
+      title: "shrib",
+      description: "online notepad that syncs based on ur slug/id. pretty nice",
+      url: "https://shrib.com/",
+    },
+    {
+      title: "cobalt",
+      description:
+        "high quality video/audio downloader from a variety of social media sites",
+      url: "https://cobalt.tools/",
+    },
+    {
+      title: "ytdlp-online",
+      description:
+        "for when cobalt doesn't cut it. supports basically every website ever",
+      url: "https://ytdlp.online/",
+    },
+  ],
+  web_dev: [
+    {
+      title: "squoosh",
+      description: "compresses images super well to a ton of different formats",
+      url: "https://squoosh.app/",
+    },
+    {
+      title: "fontie",
+      description:
+        "super cool webfont generator that i discovered while working on this site. has tons of conversion options",
+      url: "https://fontie.pixelsvsbytes.com/webfont-generator",
+    },
+    {
+      title: "fontello",
+      description:
+        "font generator that allows you to create custom fonts from a variety of icons",
+      url: "https://fontello.com/",
+    },
+    {
+      title: "google-webfonts-helper",
+      description:
+        "google webfonts helper that allows you to preview and download fonts",
+      url: "https://gwfh.mranftl.com/",
+    },
+  ],
 };
+
+export const links = [
+  {
+    title: "cari institute",
+    description:
+      "the consumer aesthetics research institute website, a catalog of various design eras",
+    url: "https://cari.institute/",
+    categories: ["design"],
+  },
+  {
+    title: "dafont",
+    description: "massive free font catalog",
+    url: "https://www.dafont.com/",
+    categories: ["design", "fonts"],
+  },
+  {
+    title: "fontshare",
+    description:
+      "another (smaller) free font catalog; tends to focus on more modern fonts",
+    url: "https://fontshare.com/",
+    categories: ["design", "fonts"],
+  },
+  {
+    title: "typerip",
+    description: "adobe fonts ripper. for testing purposes. obviously",
+    url: "https://badnoise.net/TypeRip/",
+    categories: ["design", "fonts"],
+  },
+  {
+    title: "macosicons",
+    description:
+      "exactly what it sounds like; over 26k macOS app icons (disclaimer: some are unofficial)",
+    url: "https://macosicons.com/",
+    categories: ["design"],
+  },
+  {
+    title: "what the font",
+    description:
+      "font detector. im not gon hold u the effectiveness depends on the day",
+    url: "https://www.myfonts.com/pages/whatthefont",
+    categories: ["design", "fonts"],
+  },
+  {
+    title: "miruro",
+    description: "pretty solid anime site",
+    url: "https://www.miruro.to/",
+    categories: ["apps"],
+  },
+  {
+    title: "anki",
+    description:
+      "the absolute BEST srs (spaced repitition system) ever. great form of digital flash cards",
+    url: "https://apps.ankiweb.net/",
+    categories: ["apps"],
+  },
+  {
+    title: "tinywow",
+    description:
+      "'free pdf editor online', 'free png to jpg converter online', etc, all in one place, with no ads or anything. 10/10",
+    url: "https://tinywow.com/",
+    categories: ["apps"],
+  },
+  {
+    title: "shrib",
+    description: "online notepad that syncs based on ur slug/id. pretty nice",
+    url: "https://shrib.com/",
+    categories: ["apps"],
+  },
+  {
+    title: "cobalt",
+    description:
+      "high quality video/audio downloader from a variety of social media sites",
+    url: "https://cobalt.tools/",
+    categories: ["apps"],
+  },
+  {
+    title: "ytdlp-online",
+    description:
+      "for when cobalt doesn't cut it. supports basically every website ever",
+    url: "https://ytdlp.online/",
+    categories: ["apps"],
+  },
+  {
+    title: "squoosh",
+    description: "compresses images super well to a ton of different formats",
+    url: "https://squoosh.app/",
+    categories: ["apps", "dev"],
+  },
+  {
+    title: "fontie",
+    description:
+      "super cool webfont generator that i discovered while working on this site. has tons of conversion options",
+    url: "https://fontie.pixelsvsbytes.com/webfont-generator",
+    categories: ["dev"],
+  },
+  {
+    title: "fontello",
+    description: "super cool icons font generator",
+    url: "https://fontello.com/",
+    categories: ["dev", "fonts"],
+  },
+  {
+    title: "google-webfonts-helper",
+    description:
+      "google webfonts helper that allows you to preview and download fonts",
+    url: "https://gwfh.mranftl.com/",
+    categories: ["dev"],
+  },
+  {
+    title: "astro",
+    description:
+      "the genuine best static site generator i've ever witnessed in my entire life. used to build this site!",
+    url: "https://astro.build/",
+    categories: ["dev"],
+  },
+  {
+    title: "listenbrainz",
+    description:
+      "foss listening history and stats service, used for the currently playing track and tunes page",
+    url: "https://listenbrainz.org/",
+    categories: ["apps"],
+  },
+  {
+    title: "railway (referral link!)",
+    description:
+      "the best platform-as-a-service i've ever used, hosts the now playing websocket api this site uses (i do genuinely love it)",
+    url: "https://railway.com?referralCode=yZnYgd",
+    categories: ["dev"],
+  },
+];
