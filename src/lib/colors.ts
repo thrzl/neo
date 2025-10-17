e942bfa160f89b70a04f1860b9786199791045fc
te <thrzl@icloud.com>
2025-10-17 17:33:30 -0400

    improve color lib + use LCH instead of HSV for saturation calculations

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,9 +1,9 @@
 import type { Colord, RgbaColor } from "colord";
 import { colord, extend } from "colord";
 import harmoniesPlugin from "colord/plugins/harmonies";
-import labPlugin from "colord/plugins/lab";
+import lchPlugin from "colord/plugins/lch";
 
-extend([harmoniesPlugin, labPlugin]);
+extend([harmoniesPlugin, lchPlugin]);
 
 function rgbToArray(color: RgbaColor) {
   return [color.r, color.g, color.b].join(", ");
@@ -21,6 +21,7 @@ export type CompletePalette = {
   palette: string[];
   dominant: string;
   textColor: string;
+  secondaryTextColor: string;
 };
 
 function hexToRgb(hex: string) {
@@ -41,12 +42,13 @@ function targetLuminance(bgLuminance: number, lighten = false) {
   return (bgLuminance + 0.05) / 4.5 - 0.05;
 }
 
-export function getPalette(ct_palette: RGBColor[]) {
-  let rawPalette: Colord[] = (ct_palette || defaultPalette).map(([r, g, b]) =>
-    colord({ r, g, b }),
+export function getPalette(colorThiefPalette: RGBColor[]): CompletePalette {
+  let rawPalette: Colord[] = (colorThiefPalette || defaultPalette).map(
+    ([r, g, b]) => colord({ r, g, b }),
   );
 
   if (rawPalette.length === 0) {
+    // if no rawPalette, resort to default palette
     rawPalette = defaultPalette.map(([r, g, b]) => colord({ r, g, b }));
   }
 
@@ -54,25 +56,25 @@ export function getPalette(ct_palette: RGBColor[]) {
   const rawDominant = rawPalette.shift()!;
 
   if (rawPalette.length < 3) {
+    // if not enough colors, generate a color scheme ourselves
     const harmony = rawDominant.harmonies("analogous");
+
+    // keep original dominant color and whatever other colors we have
     rawPalette = [rawDominant, ...rawPalette, ...harmony.slice(1)].slice(0, 4);
     console.log(`harmonized rawPalette: ${rawPalette}`);
   }
 
   const dominant = rawDominant.toRgb();
-  rawPalette = rawPalette.sort(
-    (a, b) => colord(a).toHsv().s - colord(b).toHsv().s,
+  const palette = rawPalette.sort(
+    (a, b) => colord(a).toLch().c - colord(b).toLch().c,
   );
   // sort palette by contrast with dominant (highest first), and then convert to rgb
-  if (getContrast(rawDominant, rawPalette[1]) < 0.2) {
-    const bgLuminance = rawDominant.brightness();
-    const secondary = rawPalette[1].toHsl();
-    // secondary.l = targetLuminance(bgLuminance, bgLuminance < 0.5);
-    rawPalette[1] = colord(secondary);
-  }
-  const palette = rawPalette
-    .sort((color) => -getContrast(rawDominant, color))
-    .map((color) => rgbToString(color.toRgb()));
+  // if (getContrast(rawDominant, rawPalette[1]) < 0.2) {
+  //   const bgLuminance = rawDominant.brightness();
+  //   const secondary = rawPalette[1].toHsl();
+  //   secondary.l = targetLuminance(bgLuminance, bgLuminance < 0.5);
+  //   rawPalette[1] = colord(secondary);
+  // }
 
   // calculate text color from dominant color luminance
   const textColor =
@@ -85,12 +87,12 @@ export function getPalette(ct_palette: RGBColor[]) {
   //   rawPalette[0] = colord(textColor);
   // }
   const secondaryTextColor =
-    rawPalette[0].brightness() > 0.4
+    palette[0].brightness() > 0.4
       ? { r: 0, g: 0, b: 0, a: 255 }
       : { r: 255, g: 255, b: 255, a: 255 };
 
   const result = {
-    palette,
+    palette: palette.map((color) => rgbToString(color.toRgb())),
     textColor: rgbToString(textColor),
     dominant: rgbToString(dominant),
     secondaryTextColor: rgbToString(secondaryTextColor),
