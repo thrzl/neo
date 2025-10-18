0082c4f2049d395e914d9c37d31ccd4e39e45ef3
te <thrzl@icloud.com>
2025-10-18 12:24:45 -0400

    move unocss prose config to uno.config.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-import { transformerDirectives, presetWind3, presetTypography } from "unocss";
 
 import sitemap from "@astrojs/sitemap";
 
@@ -10,75 +9,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    UnoCSS({
-      injectReset: true,
-      transformers: [transformerDirectives()],
-      presets: [
-        presetWind3(),
-        presetTypography({
-          cssExtend: {
-            img: {
-              "margin-left": "auto",
-              "margin-right": "auto",
-            },
-            a: {
-              "text-decoration": "underline dotted 1px var(--accent-bg)",
-              "font-style": "italic",
-            },
-            "a:hover": {
-              // "text-shadow": "0 0 0.125rem var(--accent-text, #fff)",
-              "text-decoration": "underline wavy 1px var(--accent-bg-light)",
-            },
-            'input[type="checkbox"]:checked': {
-              "background-color": "var(--accent-bg-light)",
-              /* Add checkmark if desired */
-            },
-            'input[type="checkbox"]': {
-              appearance: "none",
-              width: "1.25rem",
-              height: "1.25rem",
-              "margin-right": "0.1rem",
-              border: "2px solid var(--accent-bg-light)",
-              "background-color": "transparent",
-              "border-radius": "0.25rem",
-              cursor: "pointer",
-              display: "inline-block",
-              "vertical-align": "middle",
-            },
-            "h1, h2, h3, h4, h5, h6": {
-              "text-decoration": "underline wavy 1px var(--accent-bg)",
-              width: "max-content",
-              color: "var(--accent-text)",
-            },
-            "#footnote-label": {
-              "text-transform": "lowercase",
-            },
-            ins: {
-              "text-decoration": "underline wavy 1px",
-              "font-style": "italic",
-            },
-            "li.task-list-item": {
-              "list-style-type": "none",
-            },
-            "sup > a": {
-              "text-decoration": "none",
-              color: "var(--accent-bg) !important",
-              "font-style": "normal",
-            },
-            pre: {
-              "font-size": "0.8rem",
-            },
-            "*": {
-              color: "var(--accent-text)",
-            },
-          },
-        }),
-      ],
-    }),
-    sitemap(),
-    mdx(),
-  ],
+  integrations: [UnoCSS({ injectReset: true }), sitemap(), mdx()],
   site: "https://thrizzle.nekoweb.org",
   vite: {
     plugins: [
--- uno.config.ts
+++ uno.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "unocss";
+import { transformerDirectives, presetWind3, presetTypography } from "unocss";
 
 export default defineConfig({
   safelist: ["transition-colors", "duration-500"],
@@ -24,4 +25,66 @@ export default defineConfig({
       },
     },
   },
+  transformers: [transformerDirectives()],
+  presets: [
+    presetWind3(),
+    presetTypography({
+      cssExtend: {
+        img: {
+          "margin-left": "auto",
+          "margin-right": "auto",
+        },
+        a: {
+          "text-decoration": "underline dotted 1px var(--accent-bg)",
+          "font-style": "italic",
+        },
+        "a:hover": {
+          // "text-shadow": "0 0 0.125rem var(--accent-text, #fff)",
+          "text-decoration": "underline wavy 1px var(--accent-bg-light)",
+        },
+        'input[type="checkbox"]:checked': {
+          "background-color": "var(--accent-bg-light)",
+          /* Add checkmark if desired */
+        },
+        'input[type="checkbox"]': {
+          appearance: "none",
+          width: "1.25rem",
+          height: "1.25rem",
+          "margin-right": "0.1rem",
+          border: "2px solid var(--accent-bg-light)",
+          "background-color": "transparent",
+          "border-radius": "0.25rem",
+          cursor: "pointer",
+          display: "inline-block",
+          "vertical-align": "middle",
+        },
+        "h1, h2, h3, h4, h5, h6": {
+          "text-decoration": "underline wavy 1px var(--accent-bg)",
+          width: "max-content",
+          color: "var(--accent-text)",
+        },
+        "#footnote-label": {
+          "text-transform": "lowercase",
+        },
+        ins: {
+          "text-decoration": "underline wavy 1px",
+          "font-style": "italic",
+        },
+        "li.task-list-item": {
+          "list-style-type": "none",
+        },
+        "sup > a": {
+          "text-decoration": "none",
+          color: "var(--accent-bg) !important",
+          "font-style": "normal",
+        },
+        pre: {
+          "font-size": "0.8rem",
+        },
+        "*": {
+          color: "var(--accent-text)",
+        },
+      },
+    }),
+  ],
 });
