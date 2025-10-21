c49d2430da5152a4115056fda33b2989991e29e0
te <thrzl@icloud.com>
2025-10-21 17:26:30 -0400

    improve style consistency

--- uno.config.ts
+++ uno.config.ts
@@ -7,10 +7,7 @@ export default defineConfig({
     {
       "text-glow": "text-shadow-[0_0_0.125rem_var(--accent-bg,#fff)]",
     },
-    {
-      link: "underline decoration-2 decoration-dotted hover:text-glow hover:decoration-wavy hover:decoration-1 decoration-cover-accent !hover:decoration-cover-bright",
-    },
-    { divider: "b-muted-8 b-dotted b-8 b-b-0 h-0 p-0 my-8 w-1/4 mx-auto" },
+
     { hunderline: "b-b-dotted b-b-1 b-b-cover-accent" },
   ],
   theme: {
